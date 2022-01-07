8ea962bffc2c4737fd7a166bf6447a9a3b7b26a7
Ding <705220170@qq.com>
2022-01-07 20:17:53 +0800

    Site updated: 2022-01-07 20:17:52

---
+++ js/custom/timeCount.js
@@ -0,0 +1,61 @@
+function getdate(period) {
+	var yearLevelValue = 365 * 24 * 60 * 60 * 1000;
+	var monthLevelValue = 30 * 24 * 60 * 60 * 1000;
+	var dayLevelValue = 24 * 60 * 60 * 1000;
+	var hourLevelValue = 60 * 60 * 1000;
+	var minuteLevelValue = 60 * 1000;
+	var secondLevelValue = 1000;
+	return getDifference(period);
+	
+	function getDifference(period) {
+		/*******计算出时间差中的年、月、日、天、时、分、秒*******/
+		var year = parseInt(getYear(period));
+		var month = parseInt(getMonth(period - year * yearLevelValue));
+		var day = parseInt(getDay(period - year * yearLevelValue - month * monthLevelValue));
+		var hour = parseInt(getHour(period - year * yearLevelValue - month * monthLevelValue - day * dayLevelValue));
+		var minute = parseInt(getMinute(period - year * yearLevelValue - month * monthLevelValue - day * dayLevelValue -
+			hour * hourLevelValue));
+		var second = parseInt(getSecond(period - year * yearLevelValue - month * monthLevelValue - day * dayLevelValue -
+			hour * hourLevelValue - minute * minuteLevelValue));
+		var result = "";
+		if (year != 0) result = result + year + "年";
+		if (month != 0) result = result + month + "月";
+		if (day != 0) result = result + day + "天";
+		result = result + hour + "时" + minute + "分" + second + "秒";
+		return result;
+		
+		function getYear(period) {
+			//Math.floor()
+			return Math.floor(parseInt(period) / yearLevelValue);
+		}
+
+		function getMonth(period) {
+			return Math.floor(parseInt(period) / monthLevelValue);
+		}
+
+		function getDay(period) {
+			return Math.floor(parseInt(period) / dayLevelValue);
+		}
+
+		function getHour(period) {
+			return Math.floor(parseInt(period) / hourLevelValue);
+		}
+
+		function getMinute(period) {
+			return Math.floor(parseInt(period) / minuteLevelValue);
+		}
+
+		function getSecond(period) {
+			return Math.floor(parseInt(period) / secondLevelValue);
+		}
+		
+	}
+}
+function getdiffTime(){
+	var oldData = new Date("2020/12/31 12:00:00");
+	var newData = new Date();
+	document.getElementById("timeShow").innerHTML = getdate(newData-oldData);
+}
+//setInterval(function() {getdiffTime()},1000); 两个是一个意思
+setInterval("getdiffTime()",1000); //每隔一秒执行一次 getdiffTime() 函数
+
