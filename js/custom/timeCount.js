90acee69b50f0eda8376548e92b107a0a8ad26c2
Ding <705220170@qq.com>
2022-01-10 06:20:48 +0800

    Site updated: 2022-01-10 06:20:48

--- js/custom/timeCount.js
+++ js/custom/timeCount.js
@@ -1,1 +1,65 @@
-function getdate(t){var e=31536e6,n=2592e6,r=864e5,a=36e5,o=6e4;return function(t){var f=parseInt(function(t){return Math.floor(parseInt(t)/e)}(t)),u=parseInt(function(t){return Math.floor(parseInt(t)/n)}(t-f*e)),i=parseInt(function(t){return Math.floor(parseInt(t)/r)}(t-f*e-u*n)),I=parseInt(function(t){return Math.floor(parseInt(t)/a)}(t-f*e-u*n-i*r)),s=parseInt(function(t){return Math.floor(parseInt(t)/o)}(t-f*e-u*n-i*r-I*a)),p=parseInt(function(t){return Math.floor(parseInt(t)/1e3)}(t-f*e-u*n-i*r-I*a-s*o)),c="";0!=f&&(c=c+f+" 年 ");0!=u&&(c=c+u+" 月 ");0!=i&&(c=c+i+" 天 ");return c=(I<10?c+"0"+I:c+I)+" 时 ",c=(s<10?c+"0"+s:c+s)+" 分 ",c="本站已安全运行 "+(c=(p<10?c+"0"+p:c+p)+" 秒")}(t)}function getdiffTime(){var t=new Date("2021/12/31 12:00:00"),e=new Date;document.getElementById("timeShow").innerHTML=getdate(e-t)}setInterval("getdiffTime()",1e3);
+//页脚时间
+function getdate(period) {
+	var yearLevelValue = 365 * 24 * 60 * 60 * 1000;
+	var monthLevelValue = 30 * 24 * 60 * 60 * 1000;
+	var dayLevelValue = 24 * 60 * 60 * 1000;
+	var hourLevelValue = 60 * 60 * 1000;
+	var minuteLevelValue = 60 * 1000;
+	var secondLevelValue = 1000;
+	return getDifference(period);
+
+	function getDifference(period) {
+		/*******计算出时间差中的年、月、日、天、时、分、秒*******/
+		var year = parseInt(getYear(period));
+		var month = parseInt(getMonth(period - year * yearLevelValue));
+		var day = parseInt(getDay(period - year * yearLevelValue - month * monthLevelValue));
+		var hour = parseInt(getHour(period - year * yearLevelValue - month * monthLevelValue - day * dayLevelValue));
+		var minute = parseInt(getMinute(period - year * yearLevelValue - month * monthLevelValue - day * dayLevelValue -
+			hour * hourLevelValue));
+		var second = parseInt(getSecond(period - year * yearLevelValue - month * monthLevelValue - day * dayLevelValue -
+			hour * hourLevelValue - minute * minuteLevelValue));
+		var result = "";
+		if (year != 0) result = result + year + " 年 ";
+		if (month != 0) result = result + month + " 月 ";
+		if (day != 0) result = result + day + " 天 ";
+		result = (hour < 10 ? result + "0" + hour : result + hour) + " 时 ";
+		result = (minute < 10 ? result + "0" + minute : result + minute) + " 分 ";
+		result = (second < 10 ? result + "0" + second : result + second) + " 秒";
+		result = "本站已安全运行 " + result;
+		return result;
+
+		function getYear(period) {
+			//Math.floor()
+			return Math.floor(parseInt(period) / yearLevelValue);
+		}
+
+		function getMonth(period) {
+			return Math.floor(parseInt(period) / monthLevelValue);
+		}
+
+		function getDay(period) {
+			return Math.floor(parseInt(period) / dayLevelValue);
+		}
+
+		function getHour(period) {
+			return Math.floor(parseInt(period) / hourLevelValue);
+		}
+
+		function getMinute(period) {
+			return Math.floor(parseInt(period) / minuteLevelValue);
+		}
+
+		function getSecond(period) {
+			return Math.floor(parseInt(period) / secondLevelValue);
+		}
+
+	}
+}
+
+function getdiffTime() {
+	var oldData = new Date("2021/12/31 12:00:00");
+	var newData = new Date();
+	document.getElementById("timeShow").innerHTML = getdate(newData - oldData);
+}
+//setInterval(function() {getdiffTime()},1000); 两个是一个意思
+setInterval("getdiffTime()", 1000); //每隔一秒执行一次 getdiffTime() 函数
