f2cc943f7c44618eaaefcdb977fa305e90d464b5
Ding <705220170@qq.com>
2022-07-26 18:02:07 +0800

    Site updated: 2022-07-26 18:02:06

--- js/custom/reNameTalk.js
+++ js/custom/reNameTalk.js
@@ -1,6 +1,1 @@
-// 把 Artitalk 从碎碎念改成随言。由于需要取消pjax处理，所以单独拿出来
-function myArtitalk() {
-	document.querySelector(".item-headline a[title='artitalk page link']").innerHTML = "随言";
-}
-// 执行将碎碎念改为随言一次
-myArtitalk();
+function myArtitalk(){document.querySelector(".item-headline a[title='artitalk page link']").innerHTML="随言"}myArtitalk();
